a832448274fd844842d3428b9f28fe41c26af9a5
mikuriya <mickly9@hotmail.com>
2019-03-08 14:22:09 +0900

    add unix dgram

--- go_socket.go
+++ go_socket.go
@@ -19,7 +19,7 @@ func stream(af, dst, str string) {
     fmt.Printf("error Read %s\n", err)
     return
   }
-  println(string(buf[0:n]))
+  print(string(buf[0:n]))
 }
 
 func dgram(af, dst, str string) {
@@ -36,10 +36,35 @@ func dgram(af, dst, str string) {
   conn.Write([]byte(str))
 }
 
+func unix_dgram(af, file, str string) {
+  addr := net.UnixAddr{ file, af }
+  conn, err := net.DialUnix(af, nil, &addr)
+  if err != nil {
+    fmt.Printf("error net.DialUnix %s\n", err)
+    return
+  }
+  defer conn.Close()
+  _, err = conn.Write([]byte(str))
+  if err != nil {
+    fmt.Printf("error Write %s\n", err)
+  }
+  if af != "unix" {
+    return
+  }
+  buf := make([]byte, 1024)
+  n, err := conn.Read(buf[:])
+  if err != nil {
+    fmt.Printf("error Read %s\n", err)
+    return
+  }
+  print(string(buf[0:n]))
+}
+
 func main() {
   stream("tcp", "127.0.0.1:9002", "hello tcp")
   dgram("udp", "localhost:9001", "hello udp")
-  stream("unix", "/tmp/unix_stream.socket", "hello unix stream")
-  dgram("unix", "/tmp/unix_dgram.socket", "hello unix dgram")
+  stream("unix", "/tmp/unix_stream.socket", "hello unix stream1")
+  unix_dgram("unixgram", "/tmp/unix_dgram.socket", "hello unix dgram")
+  unix_dgram("unix", "/tmp/unix_stream.socket", "hello unix stream2")
 }
 
