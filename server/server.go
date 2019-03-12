7bdf8e42cb9a49636cf5778625c2497377b2dd00
mikuriya <mickly9@hotmail.com>
2019-03-12 13:52:32 +0900

    mod

--- server/server.go
+++ server/server.go
@@ -5,17 +5,26 @@ import (
 	"net"
 )
 
-func udp1() {
-    conn, _ := net.ListenPacket("udp", "localhost:9001")
-    defer conn.Close()
-
-    buffer := make([]byte, 1500)
-    for {
-        n, addr, _ := conn.ReadFrom(buffer)
-        fmt.Printf("Received from %v: %v\n", addr, string(buffer[:n]))
-    }
-}
-
 func main() {
-	println("hello client")
+    udp, _ := net.ListenPacket("udp", "localhost:9001")
+    defer udp.Close()
+    tcp, _ := net.Listen("tcp", ":9002")
+    defer tcp.Close()
+
+    go func(conn net.PacketConn) {
+        for {
+            buffer := make([]byte, 1500)
+            n, addr, _ := conn.ReadFrom(buffer)
+            fmt.Printf("UDP from %v: %v\n", addr, string(buffer[:n]))
+        }
+    }(udp)
+
+    for {
+        client, _ := tcp.Accept()
+        defer client.Close()
+        buffer := make([]byte, 1500)
+        n, _ := client.Read(buffer)
+        fmt.Printf("TCP %v\n", string(buffer[:n]))
+        client.Write([]byte("OK"))
+    }
 }
