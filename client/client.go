2fb531bc0d758dff46deeb3108bd8f6098df9c5a
mikuriya <mickly9@hotmail.com>
2019-03-12 13:29:23 +0900

    mod

--- client/client.go
+++ client/client.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"net"
+)
+
+func udp1() {
+    conn, _ := net.ListenPacket("udp", "localhost:9001")
+    defer conn.Close()
+
+    buffer := make([]byte, 1500)
+    for {
+        n, addr, _ := conn.ReadFrom(buffer)
+        fmt.Printf("Received from %v: %v\n", addr, string(buffer[:n]))
+    }
+}
+
 func main() {
 	println("hello client")
 }
